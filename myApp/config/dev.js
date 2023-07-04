530edeb3e75bcba6ac2b4b289c8cb4d379e2236d
jiajiabaobei <125649514+jiajiabaobei@users.noreply.github.com>
2023-07-04 16:12:08 +0800

    修复跨域问题

--- myApp/config/dev.js
+++ myApp/config/dev.js
@@ -5,5 +5,16 @@ module.exports = {
   defineConstants: {
   },
   mini: {},
-  h5: {}
+  h5: {
+    devServer: {
+        host: 'localhost',
+        port: 10086,
+        proxy: {
+            'calculate_expense': {//接口访问路径
+                target: 'http://127.0.0.1:5000',  // 服务端域名
+                changeOrigin: true
+            }
+        }
+    },
+  }
 }
--- myApp/src/pages/index/index.tsx
+++ myApp/src/pages/index/index.tsx
@@ -1,5 +1,5 @@
-import { useState } from 'react' //导入useState
-import Taro from '@tarojs/taro' //导入 Taro
+import { useState } from 'react' // 导入 useState
+import Taro from '@tarojs/taro' // 导入 Taro
 import { View, Text, Input, Button } from '@tarojs/components' // 导入 View、Text、Input 和 Button 组件
 
 function SalaryPage() {
@@ -13,19 +13,22 @@ function SalaryPage() {
   // 发送请求并更新男女支出状态
   const calculateExpense = () => {
     Taro.request({
-      url: 'http://127.0.0.1:5000/calculate_expense', // 后端接口地址
+      url: 'http://localhost:5000/calculate_expense', // 后端接口地址
       method: 'POST',
+      header: {
+        'content-type': 'application/json' // 设置请求头
+      },
       data: {
-      male_salary: maleSalary, // 发送男工资
-      female_salary: femaleSalary, // 发送女工资
-      fixed_expense: totalExpense // 发送支出总额
+        male_salary: parseFloat(maleSalary), // 发送男工资，使用 parseFloat 将字符串转换为浮点数
+        female_salary: parseFloat(femaleSalary), // 发送女工资，使用 parseFloat 将字符串转换为浮点数
+        fixed_expense: parseFloat(totalExpense) // 发送支出总额，使用 parseFloat 将字符串转换为浮点数
       },
       success: res => {
-      setMaleExpense(res.data.male_expense) // 更新男支出状态
-      setFemaleExpense(res.data.female_expense) // 更新女支出状态
+        setMaleExpense(res.data.male_expense) // 更新男支出状态
+        setFemaleExpense(res.data.female_expense) // 更新女支出状态
       },
       fail: err => {
-      console.log(err) // 打印错误信息
+        console.log(err) // 打印错误信息
       }
     })
   }
@@ -35,15 +38,15 @@ function SalaryPage() {
     <View>
       <View>
         <Text>男工资</Text>
-        <Input value={maleSalary} onInput={e => setMaleSalary(e.target.value)} /> {/* 渲染男工资输入框 */}
+        <Input value={maleSalary} onInput={e => setMaleSalary(e.detail.value)} /> {/* 渲染男工资输入框 */}
       </View>
       <View>
         <Text>女工资</Text>
-        <Input value={femaleSalary} onInput={e => setFemaleSalary(e.target.value)} /> {/* 渲染女工资输入框 */}
+        <Input value={femaleSalary} onInput={e => setFemaleSalary(e.detail.value)} /> {/* 渲染女工资输入框 */}
       </View>
       <View>
         <Text>支出总额</Text>
-        <Input value={totalExpense} onInput={e => setTotalExpense(e.target.value)} /> {/* 渲染支出总额输入框 */}
+        <Input value={totalExpense} onInput={e => setTotalExpense(e.detail.value)} /> {/* 渲染支出总额输入框 */}
       </View>
       <Button onClick={calculateExpense}>计算</Button> {/* 渲染计算按钮 */}
       {maleExpense && femaleExpense && ( // 如果男女支出都不为空，则渲染结果
